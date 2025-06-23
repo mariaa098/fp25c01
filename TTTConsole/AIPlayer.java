6bd393c12052e3cab91c9a7fe2f30a79464119cf
Muhammad-Rezka <rezkaapasha29@gmail.com>
2025-06-23 13:36:43 +0800

    testestes

--- TTTConsole/AIPlayer.java
+++ TTTConsole/AIPlayer.java
@@ -6,17 +6,14 @@ public abstract class AIPlayer {
     protected Seed mySeed;    // simbol komputer (X atau O)
     protected Seed oppSeed;   // simbol lawan
 
-    /** Konstruktor dengan referensi ke papan permainan */
     public AIPlayer(Board board) {
         this.cells = board.cells;
     }
 
-    /** Menetapkan simbol AI dan lawan */
     public void setSeed(Seed seed) {
         this.mySeed = seed;
         this.oppSeed = (seed == Seed.CROSS) ? Seed.NOUGHT : Seed.CROSS;
     }
 
-    /** Metode abstrak: mengembalikan langkah berikutnya {row, col} */
     public abstract int[] move();
 }
