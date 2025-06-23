371a45403c90572adb270610a5c5e611d3366a62
KEL1 <devinalif06@gmail.com>
2025-06-23 18:07:04 +0700

    IMAGE

--- TTTConsole/Seed.java
+++ TTTConsole/Seed.java
@@ -1,3 +1,32 @@
-public enum Seed {   // to save as "Seed.java"
-    CROSS, NOUGHT, NO_SEED
+import javax.swing.*;
+import java.awt.*;
+import java.net.URL;
+
+public enum Seed {
+    CROSS("X.png"),
+    NOUGHT("O.png"),
+    NO_SEED(null);
+
+    private final ImageIcon icon;
+
+    Seed(String imagePath) {
+        if (imagePath != null) {
+            URL imageURL = getClass().getResource(imagePath);
+            if (imageURL != null) {
+                icon = new ImageIcon(imageURL);
+            } else {
+                System.err.println("Image not found: " + imagePath);
+                icon = null;
+            }
+        } else {
+            icon = null;
+        }
+    }
+
+    public void draw(Graphics g, int x, int y, int size) {
+        if (icon != null) {
+            Image img = icon.getImage();
+            g.drawImage(img, x + 10, y + 10, size - 25, size - 25, null);
+        }
+    }
 }
