1dd31c106c65b12b5a94586e4262952d6c302a72
mariaa098 <mariaaarumm@gmail.com>
2025-06-24 13:12:18 +0700

    komen

--- TTTConsole/Cell.java
+++ TTTConsole/Cell.java
@@ -1,8 +1,4 @@
 import java.awt.*;
-
-/**
- * The Cell class models each individual cell of the game board.
- */
 public class Cell {
     public static final int SIZE = 145; // Ukuran sel persegi
     public static final int PADDING = SIZE / 5;
@@ -21,17 +17,15 @@ public class Cell {
     }
 
     public void paint(Graphics g) {
-        // Hitung posisi x dan y untuk menggambar
+
         int x = col * SIZE;
         int y = row * SIZE;
 
-        // Gambar border sel (opsional)
         g.setColor(new Color(220, 220, 220));
         g.fillRect(x, y, SIZE, SIZE);
         g.setColor(Color.GRAY);
         g.drawRect(x, y, SIZE, SIZE);
 
-        // Gambar X atau O jika ada isinya
         if (content != Seed.NO_SEED) {
             content.draw(g, x, y, SIZE);
         }
